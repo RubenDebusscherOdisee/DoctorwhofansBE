3bf979af714d3c779f290f232c87033a400be9bd
Ruben Debusscher <RubenDebusscherOdisee@users.noreply.github.com>
2018-08-17 00:27:37 +0200

    tidy php, part 2

--- php/random.php
+++ php/random.php
@@ -1,24 +1,16 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Max-Age: 1000');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-  
+    require("cors.php");
     require("connect.php"); 
     if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    	die("Connection failed: " . $conn->connect_error);
     }
-   $antwoord = [];
+	$antwoord = [];
 	$antwoord['data'] = "Geen links gevonden.";
 	mysqli_set_charset($conn,'utf8');
-	
-	$verified=false;
 	$stmt1 = $conn->prepare("SELECT * FROM Topics where Actief = 1 ORDER BY RAND() LIMIT 4");
 	if(!$stmt1){
-	    	    die("Statement preparing failed: " . $conn->error);
-
+		die("Statement preparing failed: " . $conn->error);
 	}
-	
 	if(!$stmt1->execute()){
 	    die("Statement execution failed: " . $stmt1->error);
 	}else{
@@ -27,14 +19,10 @@
 	    if($result->num_rows === 0) {
 	        $antwoord['data'] = "No Rows";
 	    }else{
-	                $antwoord['data'] = $result->fetch_all(MYSQLI_ASSOC);
-
+	        $antwoord['data'] = $result->fetch_all(MYSQLI_ASSOC);
 	    }
         echo json_encode($antwoord, JSON_UNESCAPED_UNICODE);
-   
 	}
-    
-    
     $stmt1->close();
     $conn->close();
 ?>
