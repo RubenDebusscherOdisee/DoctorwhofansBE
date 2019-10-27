05c468fe9a1bc8942f24a7d140c9bc3258a532ad
Ruben Debusscher <22316351+RubenDebusscher@users.noreply.github.com>
2019-10-27 20:26:17 +0100

    fixed csv import

--- admin/tables/alles/alles.php
+++ admin/tables/alles/alles.php
@@ -1,22 +1,12 @@
 <?php
 class tables_alles {
     function __import__csv($data, $defaultValues=array()){
-        // build an array of Dataface_Record objects that are to be inserted based
-        // on the CSV file data.
         $records = array();
-        
-        // first split the CSV file into an array of rows.
         $rows = explode("\n", $data);
         foreach ( $rows as $row ){
-            // We iterate through the rows and parse the name, phone number, and email 
-            // addresses to that they can be stored in a Dataface_Record object.
             list($A_Pagina,$A_Pagina_Type,$A_Type,$A_Waarde,$A_Level,$A_Actief,$A_Taal,$A_Klasse,$A_Hoort_Bij) = explode(',', $row);
-            $record = new Dataface_Record('alles', array());
-            
-            // We insert the default values for the record.
+            $record = new Dataface_Record('alles', array());            
             $record->setValues($defaultValues);
-            
-            // Now we add the values from the CSV file.
             $record->setValues(
                 array(
                     'A_Pagina'=>(int)$A_Pagina,
@@ -30,12 +20,8 @@ class tables_alles {
                     'A_Hoort_Bij'=>(int)$A_Hoort_Bij
                      )
                 );
-            
-            // Now add the record to the output array.
             $records[] = $record;
         }
-        
-        // Now we return the array of records to be imported.
         return $records;
     }
 }
--- admin/xataface/Dataface/RecordGrid.php
+++ admin/xataface/Dataface/RecordGrid.php
@@ -1,23 +1,5 @@
 <?php
-/*-------------------------------------------------------------------------------
- * Xataface Web Application Framework
- * Copyright (C) 2005-2008 Web Lite Solutions Corp (shannah@sfu.ca)
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *-------------------------------------------------------------------------------
- */
+
 
 /******************************************************************************************************************
  * File: 	Dataface/RecordGrid.php
@@ -71,7 +53,7 @@ class Dataface_RecordGrid {
 	var $actionCellCallbacks = array();
 	var $cellFilters = array();
 	
-	function __construct(&$records, $columns=null, $labels=null){
+	function __construct($records, $columns=null, $labels=null){
 		$this->records =& $records;
 		if ( !is_array($this->records) ){
 			throw new Exception('In Dataface_RecordGrid the first parameter is expected to be an array but received "'.get_class($records).'"', E_USER_ERROR);
@@ -80,7 +62,7 @@ class Dataface_RecordGrid {
 		$this->columns = $columns;
 		$this->labels = $labels;
 	}
-		function Dataface_RecordGrid(&$records, $columns=null, $labels=null) { self::__construct($records, $columns, $labels); }
+		function Dataface_RecordGrid($records, $columns=null, $labels=null) { self::__construct($records, $columns, $labels); }
 	
 	function addActionCellCallback($callback){
 		$this->actionCellCallbacks[] = $callback;
@@ -111,6 +93,7 @@ class Dataface_RecordGrid {
 		//print_r($columns);
 		
 		$gridContent = array();
+
 		foreach ($this->records as $record){
 			if ( $hasCallbacks ) $row[RecordGrid_ActionLabel] = '';
 			if ( is_a($record, 'Dataface_Record') or is_a($record, 'Dataface_RelatedRecord') ){
@@ -157,7 +140,7 @@ class Dataface_RecordGrid {
 		
 		if ( $this->labels === null ){
 			$this->labels = array();
-			foreach ($columns as $column){
+			foreach ($this->columns as $column){
 				if ( $column == RecordGrid_ActionLabel ){
 					$labels[$column] = '';
 					continue;
