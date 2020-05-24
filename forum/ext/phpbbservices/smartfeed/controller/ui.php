2cac8269d528f49fd638c9c186a9b05e95a8799f
Ruben Debusscher <22316351+RubenDebusscher@users.noreply.github.com>
2020-05-24 20:14:19 +0200

    fixed smarfeed bug

--- forum/ext/phpbbservices/smartfeed/controller/ui.php
+++ forum/ext/phpbbservices/smartfeed/controller/ui.php
@@ -392,7 +392,7 @@ class ui
 			'S_SMARTFEED_USER_ID' 				=> constants::SMARTFEED_USER_ID,
 			'S_SMARTFEED_USERNAMES' 			=> constants::SMARTFEED_USERNAMES,
 			'U_SMARTFEED_IMAGE_PATH'         	=> generate_board_url() . $this->ext_root_path . 'styles/all/theme/images/',
-			'UA_SMARTFEED_SITE_URL'				=> $this->helper->route('phpbbservices_smartfeed_feed_controller', array(), true, false, \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL),
+			'UA_SMARTFEED_SITE_URL'				=> generate_board_url().'/',
 			'UA_SMARTFEED_USER_ID'				=> $smartfeed_user_id,
 
 			)
