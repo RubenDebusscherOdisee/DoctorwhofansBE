731e79bbe8e9b4e7986e40dfd7d3c7c807fdfe12
Ruben Debusscher <22316351+RubenDebusscher@users.noreply.github.com>
2020-12-06 23:03:39 +0100

    Create templates for Quotes, Downloads and timelines
    Create templates for Quotes, Downloads and timelines

--- adm/modules/ckeditor/lib/ckeditor/plugins/templates/templates/default.js
+++ adm/modules/ckeditor/lib/ckeditor/plugins/templates/templates/default.js
@@ -24,7 +24,34 @@ CKEDITOR.addTemplates('default', {
   {
     title: 'Episode',
     image: 'template3.gif',
-    description: 'Default voor een episode tabel. Voeg deze toe aan een bestaande pagina.  Dit voeg de ondertitel Episodes toe en de tabel met koppen, die dan door de applicatie aangevult kunnen worden.',
-    html: '<h2>Episodes</h2><div style="overflow-x:auto"><table align="left" border="1" cellpadding="0" cellspacing="0" id="EpisodeTable"><thead><tr><th scope="col">Episode</th><th scope="col">Title</th><th scope="col">Run Time</th><th scope="col">Original Air date</th><th scope="col">UK viewers</th><th scope="col">Appreciation Index</th><th scope="col">Status</th></tr></thead><tbody></tbody></table></div>'
-  }]
+    description: 'Default voor een episode tabel. Voeg deze toe aan een bestaande pagina.  Dit voeg de ondertitel Episodes toe en de tabel met koppen, die dan door de applicatie aangevuld kunnen worden.',
+    html: '<h2>Episodes</h2><div style="overflow-x:auto"><table align="left" border="1" cellpadding="0" cellspacing="0" id="EpisodeTable"><thead><tr><th scope="col">Episode</th><th scope="col">Title</th><th scope="col">Run Time</th><th scope="col">Original Air date</th><th scope="col">Original Network</th><th scope="col">UK viewers</th><th scope="col">Appreciation Index</th><th scope="col">Status</th></tr></thead><tbody></tbody></table></div>'
+  },{
+    title: 'Downloads',
+    image: 'template3.gif',
+    description: 'Default voor een Download list. Voeg deze toe aan een bestaande pagina Onder de voetnoten, maar boven de Quotes.  Dit voeg de ondertitel Downloads toe met de div, die dan door de applicatie aangevuld kunnen worden.',
+    html: '<hr /><h2>Downloads</h2><div id="Downloads"></div>'
+  },{
+    title: 'Quotes',
+    image: 'template3.gif',
+    description: 'Default voor een Quote tile view. Voeg deze toe aan een bestaande pagina Onder de Downloads.  Dit voeg de ondertitel Quotes toe met de div, die dan door de applicatie aangevuld kunnen worden.',
+    html: '<hr /><h2>Quotes</h2><div id="Quotes"></div>'
+  },
+  {
+    title: 'Timeline Default',
+    image: 'template3.gif',
+    description: 'Default voor een Timeline. Bevat 1 Timelines div, 1 container en 2 contents (1 links 1 rechts, aan te passen door de classes om te switchen) en elk bevat standaard content',
+    html: '<div class="timeline"><div class="container right"><div class="content">  <h3>Qui ad mollit commodo officia in et consequat elit dolor eiusmod in est adipisicing proident.</h3> <h4>Magna deserunt enim commodo irure aliqua sint reprehenderit sit fugiat.</h4> <p>Exercitation irure eiusmod do magna tempor proident est in ipsum magna reprehenderit cillum.</p>  </div></div><div class="container left">  <div class="content"> <h3>Qui ad mollit commodo officia in et consequat elit dolor eiusmod in est adipisicing proident.</h3> <h4>Magna deserunt enim commodo irure aliqua sint reprehenderit sit fugiat.</h4> <p>Exercitation irure eiusmod do magna tempor proident est in ipsum magna reprehenderit cillum.</p>  </div></div></div>'
+  },{
+    title: 'Timeline Left',
+    image: 'template3.gif',
+    description: 'Voeg een Left item toe aan een bestaande tijdslijn',
+    html: '<div class="container left"><div class="content"><h3>Qui ad mollit commodo officia in et consequat elit dolor eiusmod in est adipisicing proident.</h3><h4>Magna deserunt enim commodo irure aliqua sint reprehenderit sit fugiat.</h4><p>Exercitation irure eiusmod do magna tempor proident est in ipsum magna reprehenderit cillum.</p></div></div>'
+  },{
+    title: 'Timeline Right',
+    image: 'template3.gif',
+    description: 'Voeg een Right item toe aan een bestaande tijdslijn',
+    html: '<div class="container right"><div class="content"><h3>Qui ad mollit commodo officia in et consequat elit dolor eiusmod in est adipisicing proident.</h3><h4>Magna deserunt enim commodo irure aliqua sint reprehenderit sit fugiat.</h4><p>Exercitation irure eiusmod do magna tempor proident est in ipsum magna reprehenderit cillum.</p></div></div>'
+  }
+]
 });
